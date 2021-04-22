4dfd1998ac56d6945edfec57a010d9d430a229f0
mersedehzahmatkesh <zm70525@gmail.com>
2021-04-22 16:30:23 +0430

    complete todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import data from './data';
+import './index.css';
+import LazyLoad from 'react-lazyload'; 
+
+const Loading = () =>{
+  return(
+    <div className="post loading">
+<h5>
+  Loading...
+</h5>
+    </div>
+  )
+}
+
+const Post = ({id, title, body}) =>{
+  return(
+<div className="post">
+<div className="post-img">
+  <LazyLoad once={true} placeholder={<img src={`https://picsum.photos/id/${id}/5/5`} alt="..." />}>
+  <img src={`https://picsum.photos/id/${id}/1000/1000`} alt="..." />
+  </LazyLoad> 
+      </div>
+  <div className="post-body">
+    <h4>{title}</h4>
+    <p>{body}</p>
+  </div>
+</div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Lazy load demo</h2>
+      <div>
+        {data.map(post=>(
+          <LazyLoad key={post.id} placeholder={<Loading/>}>
+            <Post key={post.id} {...post}/>
+          </LazyLoad>
+        ))}
+      </div>
     </div>
   );
 }
